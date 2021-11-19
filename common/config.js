d3d3966f3cf4db3b36677bac97b0aa50336766e7
小马总 <gomyorder@163.com>
2021-11-19 17:19:40 +0800

    处理过期域名

--- common/config.js
+++ common/config.js
@@ -1,5 +1,5 @@
-const ROOTPATH1 = "http://renwu.xiansqx.com/sqx_fast";//后台服务域名
-const ROOTPATH = "https://www.gomyorder.cn";//商品库域名还有其他代理域名
+const ROOTPATH1 = "https://renwu.xiansqx.com/sqx_fast";//后台服务域名
+const ROOTPATH = "https://taobao.xianmxkj.com";//商品库域名还有其他代理域名
 module.exports = {
     APIHOST: ROOTPATH,
     APIHOST1: ROOTPATH1,
